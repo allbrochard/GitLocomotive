0a49e963ccad7ce053e736928f95b0f996003752
dmett <dmett@10.0.2.15>
2016-12-20 09:48:49 +0100

    avec timer task

--- GitIMIE/src/imie/git/Main.java
+++ GitIMIE/src/imie/git/Main.java
@@ -2,6 +2,8 @@ package imie.git;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main {
 
@@ -10,10 +12,19 @@ public class Main {
 		System.out.println("Ca c'est un chat next gen !");
 		Calendar date = new GregorianCalendar();
 		System.out.println(date.getTime());
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			
+			@Override
+			public void run() {
+				System.out.println("Hey ! 10 sec passed");
+				
+			}
+		}, 10000);
 		
-		int fin = date.get(Calendar.SECOND) + 10;
+		//int fin = date.get(Calendar.SECOND) + 10;
 		//while (date.get(Calendar.SECOND)<fin){
-			System.out.println(date.get(Calendar.SECOND));
+			//System.out.println(date.get(Calendar.SECOND));
 		//}
 		
 
